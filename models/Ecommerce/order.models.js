91f49410cdb94574894730db4e5a89a4e2fc3ba1
Akhil092004 <git@stackblitz.com>
2024-10-11 01:54:14 +0530

    Initial commit

---
+++ models/Ecommerce/order.models.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const orderItemScehema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Prodcut',
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+const orderSchema = new mongoose.Schema(
+  {
+    orderPrice: {
+      type: Number,
+      required: true,
+    },
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    orderItems: {
+      type: [orderItemScehema],
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['PENDING', 'CANCELLED', 'DELIVERED'],
+      default: 'PENDING',
+    },
+  },
+  { timestamps: true }
+);
+
+export const Order = mongoose.model('Order', orderSchema);
---
+++ models/Todos/todo.models.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const todoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    complete: {
+      type: Boolean,
+      default: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    subTodo: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'SubTodo',
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Todo = mongoose.model('Todo', todoSchema);
